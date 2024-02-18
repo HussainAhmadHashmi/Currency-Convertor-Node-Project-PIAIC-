94830128c982908e4fa821d87f376df757a90045
Hussain Ahmad Faraz <fs5331775@gmail.com>
2024-02-18 10:57:32 +0500

    Currency Convertor Project

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
 import inquirer from "inquirer";
 let Conversion = {
     PKR: {
+        PKR: 1,
         USD: 0.0036,
         GBP: 0.0028,
-        PKR: 1
+        EURO: 0.00332,
+        INR: 0.29679
     },
     GBP: {
+        GBP: 1,
         PKR: 350.40,
         USD: 1.27,
-        GBP: 1,
+        EURO: 1.16906,
+        INR: 104.598
     },
     USD: {
+        USD: 1,
         PKR: 276.91,
         GBP: 0.79,
-        USD: 1
+        EURO: 0.92778,
+        INR: 83.0155
+    },
+    EURO: {
+        EURO: 1,
+        PKR: 300.806,
+        GBP: 0.85515,
+        USD: 1.07753,
+        INR: 89.4517
+    },
+    INR: {
+        INR: 1,
+        PKR: 3.36276,
+        GBP: 0.00956,
+        USD: 0.01205,
+        EURO: 0.01118
     }
 };
 const currency = await inquirer.prompt([
     {
         name: "from",
         type: "list",
-        choices: ["PKR", "USD", "GBP"],
+        choices: ["PKR", "USD", "GBP", "EURO", "INR"],
         message: "Enter your currency"
     },
     {
         name: "to",
         type: "list",
-        choices: ["PKR", "USD", "GBP"],
+        choices: ["PKR", "USD", "GBP", "EURO", "INR"],
         message: "Select your Conversion"
     },
     {
